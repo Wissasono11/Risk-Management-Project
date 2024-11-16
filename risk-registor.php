c84a45b8b9672be4297ffe2185850591ba23cffc
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-11-16 21:10:33 +0700

    add: complete header table

--- risk-registor.php
+++ risk-registor.php
@@ -13,41 +13,32 @@
     <title>Risk Register</title>
 </head>
 <body>
+    <!-- Table Risk Register Start -->
     <table border="1" width="1600">
         <tr>
-            <th>No</th>
-            <th>Objective / Tujuan</th>
-            <th>Proses Bisnis</th>
-            <th>Risk Category / Jenis Kelompok Risiko</th>
-            <th>Kode Risiko</th>
-            <th>Risk Event / Uraian Peristiwa Risiko</th>
-            <th>Risiko Course / Penyebab Risiko</th> 
-            <th>Sumber Risiko (Internal / Eksternal)</th>
+            <th colspan="21">Risk Register</th>
+        </tr>
+        <tr>
+            <th rowspan="2">No</th>
+            <th rowspan="2">Objective / Tujuan</th>
+            <th rowspan="2">Proses Bisnis</th>
+            <th rowspan="2">Risk Category / Jenis Kelompok Risiko</th>
+            <th rowspan="2">Kode Risiko</th>
+            <th rowspan="2">Risk Event / Uraian Peristiwa Risiko</th>
+            <th rowspan="2">Risiko Course / Penyebab Risiko</th> 
+            <th rowspan="2">Sumber Risiko (Internal / Eksternal)</th>
             <th rowspan="1" colspan="2">Severity Akibat / Potensi Kerugian</th>
             <th>Risk Owner / Pemilik Risiko</th>
             <th>Nama Dept. / Unit Terkait</th>
             <th rowspan="1" colspan="3">Score / Nilai Inherent Risk</th>
             <th rowspan="1" colspan="3">Existing Control / Pengendalian Yang Ada </th>
             <th rowspan="1" colspan="3">Score / Nilai Residual Risk </th>
-            <th>Opsi Perlakuan Risiko</th>
-            <th>Deskripsi Tindakan Mitigasi</th>
-            <th>Likelihood</th>
-            <th>Impact</th>
-            <th>Level of Risk / Tingkat Risiko</th>
+            <th rowspan="1" colspan="2">Risk Treatment </th>
+            <th rowspan="1" colspan="3">Score / Nilai Target Risk After Mitigation </th>
         </tr>
         <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
             <th>Qualitative</th>
             <th>Rp</th>
-            <th></th>
-            <th></th>
             <th>Likelihood</th>
             <th>Impact</th>
             <th>Level of Risk / Tingkat Risiko</th>
@@ -57,12 +48,15 @@
             <th>Likelihood</th>
             <th>Impact</th>
             <th>Level of Risk / Tingkat Risiko</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>Opsi Perlakuan Risiko</th>
+            <th>Deskripsi Tindakan Mitigasi</th>
+            <th>Likelihood</th>
+            <th>Impact</th>
+            <th>Level of Risk / Tingkat Risiko</th>
         </tr>
     </table>
+    <!-- Table Risk Register End -->
+
+    
 </body>
 </html>
