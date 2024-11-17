d43bbd415dfbf9605bb3d236c151835873f1f7d0
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-11-17 13:00:27 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // connect to the database
-    $databaseHost = 'localhost';
-    $databaseName = 'risk-management';
-    $databaseUsername = 'root';
-    $databasePassword = '';
-
-    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-?>
