545dd98a73bd177b41b5302a837fadb9c315ce0f
rbw-tech <radipta111@gmail.com>
2024-11-29 18:21:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login - Risk Management System</title>
-    <link rel="stylesheet" href="themes/login.css" />
+    <link rel="stylesheet" href="assets/css/login.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,700&display=swap" rel="stylesheet" />
