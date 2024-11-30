16c9b0e50f7b28d3a2ae58ccaa6641f082f9e30f
rbw-tech <radipta111@gmail.com>
2024-11-30 16:59:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 session_start(); 
 define("DEFINE_MY_ACCESS", true);
 require_once 'config/database.php';
@@ -45,10 +45,7 @@ if (isset($_POST['login'])) {
         $error = "Invalid email or password";
     }
 }
-?> -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
+?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login - Risk Management System</title>
@@ -58,8 +55,6 @@ if (isset($_POST['login'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
 <div class="container">
     <div class="header-top"></div>
     <div class="header">
@@ -95,5 +90,3 @@ if (isset($_POST['login'])) {
 </div>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-</body>
-</html>
