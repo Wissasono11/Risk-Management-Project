32cfb91b51d07f61125b4581188dbf9f367de4c8
rbw-tech <radipta111@gmail.com>
2024-11-30 15:51:22 +0700

    Delete assets directory

--- assets/js/sidebar.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#nav-toggle").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("collapsed");
-  document.querySelector(".main-content").classList.toggle("expanded");
-});
