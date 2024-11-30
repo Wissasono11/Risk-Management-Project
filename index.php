5dff5c8b82104f240795579c5820357db48bdf65
NUGRAHA18 <167722830+NUGRAHA18@users.noreply.github.com>
2024-11-30 16:52:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 session_start(); 
 define("DEFINE_MY_ACCESS", true);
 require_once 'config/database.php';
@@ -45,22 +45,31 @@ if (isset($_POST['login'])) {
         $error = "Invalid email or password";
     }
 }
-?>
+?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login - Risk Management System</title>
-    <link rel="stylesheet" href="assets/css/login.css" />
+    <link rel="stylesheet" href="./style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <header>
-        <h2 class="logo">Risk Management</h2>
-    </header>
+<div class="container">
+    <div class="header-top"></div>
+    <div class="header">
+        <div class="logo">
+            <i class="fa fa-ge"></i>
+        </div>
+        <h2 class="logo">Risk 
+        Management</h2>
+    </div>
+
     <div class="form-box login">
         <h2>Login</h2>
         <?php if (isset($error)): ?>
@@ -83,6 +92,7 @@ if (isset($_POST['login'])) {
             <button type="submit" name="login" class="btn">Login</button>
         </form>
     </div>
+</div>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
