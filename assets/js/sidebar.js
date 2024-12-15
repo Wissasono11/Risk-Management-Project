924ea690a0bc56639c711ebc7b79c0e5b9c24792
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-12-15 16:07:26 +0700

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,4 +1,27 @@
-document.querySelector("#nav-toggle").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("collapsed");
-  document.querySelector(".main-content").classList.toggle("expanded");
+// input check-box
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const mainContent = document.querySelector(".main-content");
+  const toggleBtn = document.getElementById("sidebar-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed");
+    mainContent.classList.toggle("collapsed");
+  });
+});
+
+// close button idebar untuk mobile
+document.addEventListener("DOMContentLoaded", function () {
+  const closeButton = document.querySelector(".fa-close");
+  const sidebar = document.querySelector(".sidebar");
+
+  closeButton.addEventListener("click", () => {
+    sidebar.classList.add("hide");
+  });
+
+  // Optional: Tambahkan event untuk membuka kembali sidebar jika diperlukan
+  const openButton = document.querySelector("#sidebar-toggle");
+  openButton.addEventListener("click", () => {
+    sidebar.classList.remove("hide");
+  });
 });
